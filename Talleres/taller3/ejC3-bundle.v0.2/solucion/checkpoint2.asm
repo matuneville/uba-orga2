eb5b27832c5e594dcc1cd0bcde19736cbf4f2c9c
matuneville <nevillematias@gmail.com>
2023-08-31 15:40:12 -0300

    checkpoint2

--- Talleres/taller3/ejC3-bundle.v0.2/solucion/checkpoint2.asm
+++ Talleres/taller3/ejC3-bundle.v0.2/solucion/checkpoint2.asm
@@ -79,30 +79,44 @@ alternate_sum_4_simplified:
 
 
 ; uint32_t alternate_sum_8(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t x4, uint32_t x5, uint32_t x6, uint32_t x7, uint32_t x8);
-; registros y pila: x1[rdi], x2[rsi], x3[rdx], x4[rcx], x5[r8], x6[r9], x7[pila], x8[pila2]
+; registros y pila: x1[rdi], x2[rsi], x3[rdx], x4[rcx], x5[r8d], x6[r9d], x7[rbp + 0x10], x8[rbp + 0x18]
 alternate_sum_8:
 	;prologo
-	push	rbp
-	mov 	rsp, rbp
+	push	rbp ;
+	mov 	rbp, rsp
+	sub		rsp, 0x18
 	; COMPLETAR
-	push 	r12
-	push 	r13
-	call alternate_sum_4
-	mov		rbx, rax
-	mov		rdi, r12
-	mov		rsi, r13
-	
+	mov 	rax, rdi
+	sub		rax, rsi 
+	add		rax, rdx 
+	sub		rax, rcx  
+	add		rax, r8
+	sub		rax, r9
 
+	mov		r9, [rbp + 0x10] 
+	mov		r8, [rbp + 0x18]
+	add		rax, r9
+	sub		rax, r8
 	;epilogo
-	
+	add		rsp, 0x18
 	pop		rbp
 	ret
 
-
 ; SUGERENCIA: investigar uso de instrucciones para convertir enteros a floats y viceversa
 ;void product_2_f(uint32_t * destination, uint32_t x1, float f1);
-;registros: destination[?], x1[?], f1[?]
+;registros: destination[rax], x1[rdi], f1[xmm0]
 product_2_f:
+	;prologo
+	push	rbp ; alinea
+	mov 	rsp, rbp
+
+	; funcion
+	
+
+
+	;epilogo
+	add     rsp, 0x08
+	pop		rbp
 	ret
 
 
--- Talleres/taller3/ejC3-bundle.v0.2/solucion/main.c
+++ Talleres/taller3/ejC3-bundle.v0.2/solucion/main.c
@@ -8,8 +8,8 @@
 
 int main (void){
 	/* Acá pueden realizar sus propias pruebas */
-	assert(alternate_sum_4_using_c(8,2,5,1) == 9);	
+	// assert(alternate_sum_8(8,2,5,1,8,2,5,1) == 30);
+	uint32_t res = alternate_sum_8(10, 10, 20, 5, 10, 10, 20, 5);
+	printf("%d \n", res);
 	return 0;    
 }
-
-
