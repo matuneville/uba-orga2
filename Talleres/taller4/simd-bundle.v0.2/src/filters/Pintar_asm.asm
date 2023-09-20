3cd052b6bf73f69a033045ac1b94c6e0b16ba235
winga35 <94922215+winga35@users.noreply.github.com>
2023-09-19 21:27:00 -0300

    Update Pintar_asm.asm

--- Talleres/taller4/simd-bundle.v0.2/src/filters/Pintar_asm.asm
+++ Talleres/taller4/simd-bundle.v0.2/src/filters/Pintar_asm.asm
@@ -1,16 +1,15 @@
 global Pintar_asm
 
-section .rodata
-	.align 16  # Alinea la sección a 16 bytes (tamaño de un registro XMM)
+section .data
 
-	color_negro: .quad 0x000000FF000000FF000000FF000000FF
+	color_negro: dq 0xFF000000FF000000,  0xFF000000FF000000
 
-	color_blanco: .quad 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+	color_blanco: dq 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF
 
-	color_negro_blanco: .quad 0x000000FF000000FFFFFFFFFFFFFFFFFF
+	color_negro_blanco: dq 0xFF000000FF000000, 0xFFFFFFFFFFFFFFFF
 
-	color_blanco_negro: .quad 0xFFFFFFFFFFFFFFFF000000FF000000FF
-
+	color_blanco_negro: dq 0xFFFFFFFFFFFFFFFF, 0xFF000000FF000000
+section .text
 ;void Pintar_asm(unsigned char *src, 	[RDI]     
 ;              unsigned char *dst,		[RSI]
 ;              int width,				[RDX]
@@ -25,21 +24,20 @@ Pintar_asm:
 	push rbx ; usamos RBX para offset de columna
 	xor rbx, rbx
 
-	push r12 ; R12 va a guardar width*4
-	push r13 ; guardo RSI para no perder el primer PIXEL del destino REVISAR SI SE PUEDE SACAR
+	;push r12 ; R12 va a guardar width*4
+	push r13 ;
 	push r14 ; contador de linea
 	
 
 	; inicializo las variables
-	mov r12, rdx
-	mul r12, 4
-	mov r13, rsi
+	mov r13, r8
+	sub r13, 16
 	xor r14, r14
 
-	movdqa  xmm1, [color_negro] ; pixel negro
-	movdqa  xmm2, [color_blanco] ; pixel blanco
-	movdqa	xmm3, [color_negro_blanco]; pixel negro-blanco
-	movdqa  xmm4, [color_blanco_negro] ; pixel blanco-negro
+	movdqu  xmm1, [color_negro] ; pixel negro
+	movdqu  xmm2, [color_blanco] ; pixel blanco
+	movdqu	xmm3, [color_negro_blanco]; pixel negro-blanco
+	movdqu  xmm4, [color_blanco_negro] ; pixel blanco-negro
 
 	; PSEUDOCODIGO
 	; while height > 0{
@@ -66,22 +64,21 @@ Pintar_asm:
 		jle pinto_fila_negro
 
 		; if linea h-2 o h-1, o sea ultimas dos
-		cmp r14, rcx - 2
+		mov rax, rcx
+		sub rax, 2
+		cmp r14, rax
 		jge pinto_fila_negro
-
-		; pinto izquierda de negro-blanco
+		;pinto izquierda de negro-blanco
+		
 		cmp rbx, 0
-		je pinto_prim_col_negro ; aca el rbx no hay que retornarlo a 0, asi sigue en la misma, ok?
-
-		; pinto derecha de blanco-negro
-		cmp rbx, r12 - 16 ;
-		je pinto_ult_col_negro
+		je pinto_prim_col_negro 
+		
+		jmp pinto_fila_blanco
 
 		
 	end:
 		pop r14
 		pop r13
-		pop r12
 		pop rbx
 		pop rbp
 		ret
@@ -89,7 +86,7 @@ Pintar_asm:
 
 	
 	pinto_fila_negro:
-		cmp rbx, r12
+		cmp rbx, r8
 		jne sigo ; si no termine linea, pinto de negro
 
 		xor rbx, rbx
@@ -114,6 +111,19 @@ Pintar_asm:
         inc     r14
         add     rsi, r9
         jmp ciclo
+
+	pinto_fila_blanco:
+
+		cmp rbx, r13
+		jne sigo2 ; si no termine linea, pinto de blanco
+
+		jmp pinto_ult_col_negro
+			
+		sigo2:
+		movdqu [rsi+rbx], xmm2
+		add rbx, 16
+		jmp pinto_fila_blanco
+
 		
 
 
